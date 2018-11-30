0c0a0a9c32f0bf08c9925ea266139c2612eb03db
Leo <leoshch@gmail.com>
2018-11-30 23:32:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo su
 yum upgrade -y
-yum install docker -y
+yum install docker mail -y
 service docker start
 wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh | bash > outkey.txt
-cat outkey.out | mail
+cat outkey.txt | mail -s "Outline output" leo_shan@outlook.com
