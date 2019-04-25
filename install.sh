0f2ad3c032e1169a5f08dff346a81ff99b9e6255
Leo <leoshch@gmail.com>
2019-04-25 10:57:18 +0000

    Update install.sh
    update install  mail cmd

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
+#Amazon linux 2 AMI
 #!/bin/bash
 sudo su
 yum upgrade -y
-yum install docker mail -y
+yum install docker sendmail mail -y
 service docker start
 wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh | bash > outkey.txt
 cat outkey.txt | mail -s "Outline install output" leo_shan@outlook.com
