77e4d5afc0c4e3a936fb2b10469d18f4f11c9295
Leo <leoshch@gmail.com>
2019-09-19 12:00:22 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo su
 yum upgrade -y
 yum install docker sendmail mail -y
 service docker start
-wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh | bash > outkey.txt
+#wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh | bash > outkey.txt
+sudo bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)" > outkey.txt
 cat outkey.txt | mail -s "Outline install output" leo_shan@outlook.com
 cat /opt/outline/access.txt | mail -s "Outline key" leo_shan@outlook.com
