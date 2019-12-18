c7f9a369b3e55c0e96cf9a2a2db87d77072fa7cf
Leo <leoshch@gmail.com>
2019-12-18 15:49:33 +0800

    restart server auto restart outline server

--- install_on_ubuntu.sh
+++ install_on_ubuntu.sh
@@ -7,3 +7,4 @@ sudo service docker start
 sudo bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)" > outkey.txt
 cat outkey.txt | mail -s "Outline install output" leo_shan@outlook.com
 cat /opt/outline/access.txt | mail -s "Outline key" leo_shan@outlook.com
+systemctl enable docker.service
